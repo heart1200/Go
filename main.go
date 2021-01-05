75f994658e12b23240e39439997030dc6faec3da
Luodayu <heart1200@github.com>
2021-01-05 11:44:16 +0800

    delect some message

--- main.go
+++ main.go
@@ -1,10 +1,5 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 1.
-
 // Helloworld is our first Go program.
-//!+
+
 package main
 
 import "fmt"
@@ -12,5 +7,3 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, 世界")
 }
-
-//!-
